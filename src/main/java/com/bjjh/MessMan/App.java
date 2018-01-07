1c273dab840aa10f892f17afab913b004c9322b8
ChiSaiM <944125621@qq.com>
2018-01-07 17:33:28 +0800

    Add

---
+++ src/main/java/com/bjjh/MessMan/App.java
@@ -0,0 +1,17 @@
+package com.bjjh.MessMan;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+
+        System.getProperty("user.dir");
+
+        System.out.println(System.getProperty("user.dir"));
+    }
+}
